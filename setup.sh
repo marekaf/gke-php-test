674872999cf996d9d2f71a333f45b1f8b8a11165
Marek Bartik <marek.bartik@ackee.cz>
2018-02-23 16:05:30 +0100

    change cluster name

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+PROJECT="$(gcloud projects describe \
+    $(gcloud config get-value core/project -q) --format='get(projectNumber)')"
+
+gcloud projects add-iam-policy-binding $PROJECT \
+    --member=serviceAccount:$PROJECT@cloudbuild.gserviceaccount.com \
+    --role=roles/container.developer
