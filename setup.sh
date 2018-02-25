f7d865927d57a81d53a2ad7d89d7e7a8077eaf60
Marek Bartik <marek.bartik@ackee.cz>
2018-02-25 15:32:35 +0100

    playing with ingress

--- setup.sh
+++ setup.sh
@@ -6,3 +6,12 @@ PROJECT="$(gcloud projects describe \
 gcloud projects add-iam-policy-binding $PROJECT \
     --member=serviceAccount:$PROJECT@cloudbuild.gserviceaccount.com \
     --role=roles/container.developer
+
+kubectl autoscale deployment nginx --min 1 --max 5 --cpu-percent 1
+
+gcloud container builds list --ongoing
+
+
+kubectl autoscale deployment nginx --min 1 --max 5 --cpu-percent 1
+watch 'kubectl get hpa'
+watch 'kubectl top pods --containers | sort'
